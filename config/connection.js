adb2f14bace8b4977346ad38d5a7ae7aa83e659d
unknown <hampton.muriel@gmail.com>
2019-06-06 21:54:26 -0400

    connection for jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  port     : 3307,
-  database : 'burgers_db'
-});
+if (process.env.JAWDB_URL){
+  connection = mysql.createConnection(process.env.JAWDB_URL);
+}
+else{
+  connection= mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    port     : 3307,
+    database : 'burgers_db'
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
